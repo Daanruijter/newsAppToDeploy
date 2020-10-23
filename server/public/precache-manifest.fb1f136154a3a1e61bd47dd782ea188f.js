db7a279fc329cdeb9f300446236a75e45ff2031d
Daan Ruijter <daan_ruijter@hotmail.com>
2020-10-23 15:52:42 +0200

    commit to Heroku

---
+++ server/public/precache-manifest.fb1f136154a3a1e61bd47dd782ea188f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509004f17dcf4095f5b9",
+    "url": "/css/about.15a1c2ad.css"
+  },
+  {
+    "revision": "968f326bc0aebe3b4bab",
+    "url": "/css/app.95c3957a.css"
+  },
+  {
+    "revision": "c76c9025655f99ab3d5f746ccd09667e",
+    "url": "/fonts/steelfishrg.c76c9025.ttf"
+  },
+  {
+    "revision": "3e3970a828fe7440f60e86d3bd8a8503",
+    "url": "/img/globe.3e3970a8.jpg"
+  },
+  {
+    "revision": "42f647a0311a056243d1352cf7d06b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "509004f17dcf4095f5b9",
+    "url": "/js/about.b76145e8.js"
+  },
+  {
+    "revision": "968f326bc0aebe3b4bab",
+    "url": "/js/app.a14afef7.js"
+  },
+  {
+    "revision": "316ab5ca9d76a19df1d0",
+    "url": "/js/chunk-vendors.ef2f301d.js"
+  },
+  {
+    "revision": "05266b13c8f8dfb5365472302ecb727d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "a864d1f762421ec9e5460441f5af0774",
+    "url": "/robots.txt"
+  }
+]);
---
+++ server/server.js
@@ -0,0 +1,59 @@
+// Server.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const mongoose = require("mongoose");
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/", require("./worldNewsUrls.ts"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server is running on " + port + "port");
+});
+
+//Handle production
+if (process.env.NODE_ENV === "prouction") {
+  //Static folder
+  app.use(express.static(__dirname, +"/public"));
+
+  //Handle Single Page Application
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
+//Connect to MongoDB
+const db =
+  "mongodb+srv://dr:sSXGDQnstqEWcLHx@cluster0-4ie2c.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    dbName: "bookstore-app",
+  })
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch((err) => console.log(err + "There is no connection"));
+
+mongoose.set("debug", true);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
