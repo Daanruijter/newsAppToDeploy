52f90fbc01039d4f6d44f420c2153dcf71027f33
Daan Ruijter <daan_ruijter@hotmail.com>
2020-10-27 09:56:19 +0100

    debugging

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname, +"/public"));
 
   //Handle Single Page Application
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+  app.get("*", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
 //Connect to MongoDB
