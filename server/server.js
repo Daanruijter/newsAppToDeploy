7a4c2d4960e3db0b847a05012c5a8fffbca9565e
Daan Ruijter <daan_ruijter@hotmail.com>
2020-10-28 10:27:37 +0100

    debugging

--- server/server.js
+++ server/server.js
@@ -34,7 +34,9 @@ if (process.env.NODE_ENV === "production") {
   // });
 
   //Handle Single Page Application
-  // app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+  app.get(/.*/, (req, res) =>
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+  );
 }
 
 //Connect to MongoDB (maybe using this Mongo connection later on)
