a594f0138ee55a77517a65264aea90b4ceb36065
Daan Ruijter <daan_ruijter@hotmail.com>
2020-10-28 23:01:04 +0100

    changed writefile path sitemap to ./sitemap

--- server/worldNewsUrls.js
+++ server/worldNewsUrls.js
@@ -406,7 +406,7 @@ router.post("/updateXMLSitemap", (req, res) => {
   // Write updated XML string to the sitemap
   if (req.body.XMLDataAndEnvironMentData.environmentData === "production") {
     console.log("WRTIEFILE");
-    fs.writeFile("sitemap.xml", XMLData, (err) => {
+    fs.writeFile("./sitemap.xml", XMLData, (err) => {
       if (err) {
         console.log(err);
         res.send(err);
