fd4120c122a363d3dde060799588c03f25310739
Daan Ruijter <daan_ruijter@hotmail.com>
2020-10-28 11:34:46 +0100

    debugging

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(bodyParser.json());
@@ -33,7 +34,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
